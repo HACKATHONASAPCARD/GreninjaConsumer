15118b876c6636198e5b4aff0c0cd3c869b92202
amandinhacruz <amandaellem2023@gmail.com>
2024-02-04 16:19:01 -0300

    regras do status

--- src/main/java/com/hackathon/asapcardConsumer/AsapcardConsumerApplication.java
+++ src/main/java/com/hackathon/asapcardConsumer/AsapcardConsumerApplication.java
@@ -2,7 +2,9 @@ package com.hackathon.asapcardConsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.hackathon.repository")
 @SpringBootApplication
 public class AsapcardConsumerApplication {
 
